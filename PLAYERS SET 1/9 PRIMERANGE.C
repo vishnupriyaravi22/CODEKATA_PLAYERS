958931474c2101f28faaf952f4bd318676cfa026
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2018-03-11 01:56:15 -0800

    Rename 9 PRIME IN RANGE.C to 9 PRIMERANGE.C

---
+++ PLAYERS SET 1/9 PRIMERANGE.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+   int i,j,l,h,flag=0,count=0;
+   scanf("%d %d",&l,&h);
+   for(i=l;i<=h;i+2)
+   {
+       for(j=2;j<=i/2;j++)
+       {
+           if(i%j!=0)
+           
+               flag=1;
+               else
+               flag=0;
+           }
+           if(flag==1)
+           count++;
+       }
+   
+printf("%d",count);
+    return 0;
+}
+
